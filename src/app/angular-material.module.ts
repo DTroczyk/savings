7be7a935f142100d6f8a9eb29a72ee0ce46bc66f
DTroczyk <DTroczyk@gmail.com>
2024-09-13 10:05:18 +0200

    add empty navigation component

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
 })
-export class AngularMaterialModule { }
+export class AngularMaterialModule {}
